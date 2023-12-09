f4e0d114c798075ea4173360f5d8e99fa00fdc42
Joel Johnston <me@joelj.ca>
2023-12-09 13:35:30 -0700

    Kinda flesh out style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import '@/assets/css/theme.min.css';
+import '@/assets/vendor/boxicons/css/boxicons.min.css';
+import '@/assets/vendor/swiper/swiper-bundle.min.css';
+import Header from './ui/header';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +20,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+
+      <body className={inter.className}>
+        <Header />
+        <main className="page-wrapper">
+          {children}
+        </main>
+      </body>
     </html>
   )
 }
---
+++ src/app/ui/header.tsx
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <header className="header navbar navbar-expand-lg bg-light shadow-dark-mode-none shadow-sm">
+      <h1>Test1234</h1>
+    </header>
+  )
+}
