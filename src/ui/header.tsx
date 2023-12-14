7c85d0c76ab62941a3f3760de5e2d5bd464f58bd
Joel Johnston <me@joelj.ca>
2023-12-13 19:26:52 -0700

    Hamburger menu for mobile

--- src/ui/header.tsx
+++ src/ui/header.tsx
@@ -1,6 +1,8 @@
+"use client";
 import Link from "next/link";
 import HeaderNavLink from "./header-nav-link";
 import ThemeToggle from "./theme-toggle";
+import { useState } from "react";
 
 export interface NavigationItem {
   text: string;
@@ -9,13 +11,15 @@ export interface NavigationItem {
 };
 
 const navItems: NavigationItem[] = [
-  {text: "Services", link: "/services", bypassRouter: false},
-  {text: "Blog", link: "/blog", bypassRouter: false},
-  {text: "About Me", link: "/pages/about-me", bypassRouter: false},
-  {text: "Source", link: "https://github.com/joelj1995/joeljdotca", bypassRouter: true},
+  { text: "Services", link: "/services", bypassRouter: false },
+  { text: "Blog", link: "/blog", bypassRouter: false },
+  { text: "About Me", link: "/pages/about-me", bypassRouter: false },
+  { text: "Source", link: "https://github.com/joelj1995/joeljdotca", bypassRouter: true },
 ];
 
 export default function Header() {
+
+  const [showMenu, setShowMenu] = useState(false);
 
   return (
     <header className="header navbar navbar-expand-lg bg-light shadow-dark-mode-none shadow-sm">
@@ -27,13 +31,43 @@ export default function Header() {
           Joel Johnston
         </Link>
 
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          {navItems.map((item, index) => (
-            <HeaderNavLink navItem={item} key={index} />
-          ))}
-        </ul>
+        <div
+          id="navbar"
+          className={`offcanvas offcanvas-end bg-dark ${showMenu ? 'show' : ''}`}>
+
+          <div className="offcanvas-header border-bottom border-light">
+            <h5 className="offcanvas-title text-white">Menu</h5>
+            <button
+              type="button"
+              className="btn-close btn-close-white"
+              onClick={() => setShowMenu(false)}>
+            </button>
+          </div>
+
+          <div className="offcanvas-body">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              {navItems.map((item, index) => (
+                <HeaderNavLink navItem={item} key={index} />
+              ))}
+            </ul>
+          </div>
+
+        </div>
 
         <ThemeToggle />
+
+        <button
+          type="button"
+          className="navbar-toggler"
+          onClick={() => setShowMenu(true)}>
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div
+          className="offcanvas-backdrop fade show"
+          style={{ display: showMenu ? 'block' : 'none' }}
+          onClick={() => setShowMenu(false)}>
+        </div>
 
       </div>
     </header>
