847fbe373cd5b881e8229b5546c1c38f12b20a82
Joel Johnston <me@joelj.ca>
2023-12-16 12:20:46 -0700

    Try getting grid working

--- src/app/(dark)/hero-tagline.tsx
+++ src/app/(dark)/hero-tagline.tsx
@@ -8,7 +8,7 @@ export default function HeroTagline(props : HeroTaglineProps) {
   return (
     <div
       className="d-flex align-items-center justify-content-center bg-secondary rounded-3"
-      style={{ minHeight: ' 176px', backdropFilter: 'blur(6px)' }}>
+      style={{ minHeight: '176px', minWidth: '50%', backdropFilter: 'blur(6px)' }}>
       <div className="p-xl-4 p-sm-3 p-2 fs-xl fw-semibold text-center">
         <span className={`hero-animation-fade  text-white ${delayClass}`}>
           { props.tagline }
