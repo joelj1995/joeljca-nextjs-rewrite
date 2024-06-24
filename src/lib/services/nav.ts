d7e96840e8f811e3f7750016fb547fde0161a8a9
Joel Johnston <me@joelj.ca>
2024-06-24 15:01:30 -0600

    Remove services link

--- src/lib/services/nav.ts
+++ src/lib/services/nav.ts
@@ -7,7 +7,7 @@ export interface NavigationItem {
 
 export const navItems: NavigationItem[] = [
   // { text: "Skills", link: "/skills", bypassRouter: false },
-  { text: "Services", link: "/services", bypassRouter: false },
+  // { text: "Services", link: "/services", bypassRouter: false },
   { text: "Blog", link: "/blog", bypassRouter: false },
   { text: "About Me", link: "/pages/about-me", bypassRouter: false },
   { text: "Source", link: "https://github.com/joelj1995/joeljca-nextjs-rewrite", bypassRouter: true },
